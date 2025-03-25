3cf8ce83c3324f57af7f839d50668f104f285fc9
Lee Seungho <dooftac712091@icloud.com>
2025-03-25 17:56:03 +0900

    Implement project post

---
+++ src/main/java/com/seu/toolboxtalk/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.seu.toolboxtalk.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+
+
+
+}
